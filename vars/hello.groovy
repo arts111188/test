9a9813fada0f97ea3e2dde4aaabdcfd8f963effe
arts111188 <40269067+arts111188@users.noreply.github.com>
2020-01-09 10:27:23 +0400

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,25 @@
-def printHello() {
-    sh 'echo "hello world from library"'
+def call(int buildNumber) {
+  if (buildNumber % 2 == 0) {
+    pipeline {
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
+        }
+      }
+    }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('Odd Stage') {
+          steps {
+            echo "The build number is odd"
+          }
+        }
+      }
+    }
+  }
 }
